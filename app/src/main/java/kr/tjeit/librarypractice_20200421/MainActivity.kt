346526def45076c4453a060a976a23b5706c0425
oasis99797 <oasis99797@gmail.com>
2020-04-22 20:03:38 +0900

    전화 걸기 기능 작성

--- app/src/main/java/kr/tjeit/librarypractice_20200421/MainActivity.kt
+++ app/src/main/java/kr/tjeit/librarypractice_20200421/MainActivity.kt
@@ -1,8 +1,14 @@
 package kr.tjeit.librarypractice_20200421
 
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.bumptech.glide.Glide
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
@@ -15,6 +21,32 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        callBtn.setOnClickListener {
+
+            val permissionListener = object : PermissionListener {
+                override fun onPermissionGranted() {
+//                    사용자가 권한을 승인해준 상태일때.
+                    val uri = Uri.parse("tel:01051123237")
+                    val myIntent = Intent(Intent.ACTION_CALL, uri)
+                    startActivity(myIntent)
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+//                    사용자 권한을 거부한 상태.
+                    Toast.makeText(mContext, "권한 허용 필요", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+
+            TedPermission.with(mContext)
+                .setPermissionListener(permissionListener)
+                .setDeniedMessage("설정 > 권한에서 권한을 바꿔주세요.")
+                .setPermissions(Manifest.permission.CALL_PHONE)
+                .check()
+
+
+        }
 
     }
 
